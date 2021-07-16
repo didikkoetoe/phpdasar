122fc15ee7113a0fd2dd9a33750dc0ebf9075f70
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-07-16 18:47:52 +0700

    Menambahkan pengecekan session dan tombol logout

--- pertemuan16/index.php
+++ pertemuan16/index.php
@@ -1,18 +1,25 @@
 <?php
 
-if (isset($_POST["submit"])) {
-    // Cek apakah username dan password benar
-    if ($_POST["username"] == "admin" && $_POST["password"] == "admin") {
-        // Jika benar alihkan ke halaman admin.php
-        header("Location: admin.php");
-        exit;
-    } else {
-        // Jika salah set variable error
-        $error = true;
-    }
+session_start();
+
+if (!isset($_SESSION["login"])) {
+    header("Location: login.php");
+    exit;
+}
+
+// Hubungkan dengan halaman tempat menyimpan function
+require "function.php";
+
+$books = read("SELECT * FROM buku");
+// Diatas sudah menghasilkan array numeric
+
+// cek apakah tombol cek di pencet atau belum
+if (isset($_POST["cari"])) {
+    $books = search($_POST["keyword"]);
 }
 
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -21,25 +28,51 @@ if (isset($_POST["submit"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Admin</title>
 </head>
 
 <body>
-    <h1>Login</h1>
+    <h1>Selamat datang Admin</h1>
 
-    <?php if (isset($error)) : ?>
-        <p>Username / Password salah !</p>
-    <?php endif; ?>
+    <a href="logout.php">Logout</a>
 
     <form action="" method="POST">
-        <label for="username">Username : </label>
-        <input type="text" name="username" id="username">
-        <br><br>
-        <label for="password">Password : </label>
-        <input type="password" name="password" id="password">
-        <br><br>
-        <button type="submit" name="submit">Masuk</button>
+        <input type="text" name="keyword" size="40px" autocomplete="off" autofocus placeholder="Masukan keyword pencarian....">
+        <button type="submit" name="cari">Cari</button>
     </form>
+
+    <a href="tambah.php">Tambahkan Buku</a>
+
+    <table border="1" cellspacing="0" cellpadding="10">
+        <tr>
+            <th>No</th>
+            <th>Aksi</th>
+            <th>Judul</th>
+            <th>Pengarang</th>
+            <th>Tahun</th>
+            <th>Penerbit</th>
+            <th>Gambar</th>
+        </tr>
+
+        <?php $i = 1; ?>
+        <?php foreach ($books as $book) : ?>
+            <tr>
+                <td><?= $i; ?></td>
+                <td>
+                    <a href="hapus.php?id=<?= $book["id"] ?>" onclick="return confirm('Apakah anda yakin untuk mengapus data ini ?');">Hapus</a> |
+                    <a href="edit.php?id=<?= $book["id"] ?>">Edit</a>
+                </td>
+                <td><?= $book["judul"]; ?></td>
+                <td><?= $book["pengarang"]; ?></td>
+                <td><?= $book["tahun"]; ?></td>
+                <td><?= $book["penerbit"]; ?></td>
+                <td>
+                    <img src="../image/<?= $book['gambar']; ?>" alt="" width="100">
+                </td>
+            </tr>
+            <?php $i++; ?>
+        <?php endforeach; ?>
+    </table>
 
 </body>
 
