d2a344638d6ac13fd673efa87cb818312075dac5
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-07-16 18:50:29 +0700

    Menambahkan pengecekan session

--- pertemuan16/hapus.php
+++ pertemuan16/hapus.php
@@ -1,5 +1,12 @@
 <?php
 
+session_start();
+
+if (!isset($_SESSION["login"])) {
+    header("Location: login.php");
+    exit;
+}
+
 require "function.php";
 
 $id = $_GET["id"];
@@ -7,11 +14,11 @@ $id = $_GET["id"];
 if (delete($id) > 0) {
     echo "<script>
         alert('Data berhasil di hapus');
-        document.location.href = 'admin.php';
+        document.location.href = 'index.php';
         </script>";
 } else {
     echo "<script>
         alert('Data gagal di hapus');
-        document.location.href = 'admin.php';
+        document.location.href = 'index.php';
         </script>";
 }
