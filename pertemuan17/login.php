1a89ab9f08278cbedb358198d4dbd5a71d329b9c
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-07-17 18:11:59 +0700

    Duplicate dari pertemuan16

---
+++ pertemuan17/login.php
@@ -0,0 +1,75 @@
+<?php
+
+session_start();
+
+if (isset($_SESSION["login"])) {
+    header("Location: index.php");
+    exit;
+}
+
+require "function.php";
+
+if (isset($_POST["submit"])) {
+    // Tangkap data
+    $username = strtolower(htmlspecialchars($_POST["username"]));
+    $password = $_POST["password"];
+
+    // Cek apakah username benar
+    if ($cek = read("SELECT * FROM users WHERE username = '$username'")) {
+        // cek password
+        if (password_verify($password, $cek[0]["password"])) {
+            // ! Set $_SESSION
+            $_SESSION["login"] = true;
+
+            // Jika benar arahkan ke halaman index
+            header("Location: index.php");
+            exit;
+        }
+    }
+
+    $error = true;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <style>
+        label {
+            display: block;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Halaman Login</h1>
+
+    <?php if (isset($error)) : ?>
+        <p style="color: red; font-style:italic;">Username / Password salah</p>
+    <?php endif; ?>
+
+    <form action="" method="POST">
+        <ul>
+            <li>
+                <label for="username">Usernmae : </label>
+                <input type="text" name="username" id="username">
+            </li>
+            <li>
+                <label for="password">Password : </label>
+                <input type="password" name="password" id="password">
+            </li>
+            <li>
+                <button type="submit" name="submit">Login</button>
+            </li>
+        </ul>
+    </form>
+
+</body>
+
+</html>
