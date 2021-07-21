54ee1cb39bc138a9a97ae06c58838af73fdab8da
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-07-21 16:00:47 +0700

    memindahkan scriptnya ke signup.js

--- pertemuan19/signup.php
+++ pertemuan19/signup.php
@@ -79,55 +79,7 @@ if (isset($_POST['register'])) {
     </div>
 
     <script src="js/bootstrap.min.js"></script>
-    <script>
-        let login = $(document).ready(function() {
-            $("#form").submit(function(e) {
-                // e.preventDefault();
-
-                let username = $("#username").val();
-                let jmlUsername = username.length
-                if (jmlUsername == 0) {
-                    $("small:first").addClass("pesan");
-                    $("#username").addClass("error");
-                } else if (jmlUsername < 4) {
-                    $("small:first").text("Minimal 4 karakter !");
-                    // return false;
-                } else {
-                    $("#username").addClass("success");
-                    $("small:first").addClass("pSuccess");
-                }
-
-                let email = $("#email").val();
-                let jmlEmail = email.length;
-                if (jmlEmail === 0) {
-                    $("#email").addClass("error");
-                    $("small:eq(1)").addClass("pesan");
-                } else {
-                    $("#email").addClass("success");
-                    $("small:eq(1)").addClass("pSuccess");
-                }
-
-                let password = $("#password").val();
-                let password2 = $("#password2").val();
-                if (password == '' || password2 == '') {
-                    $("#password , #password2").addClass("error");
-                    $("small:eq(2)").addClass("pesan");
-                    $("small:eq(3)").addClass("pesan");
-                    return false;
-                } else if (password != password2) {
-                    $("#password , #password2").addClass("error");
-                    $("small:eq(2)").addClass("pesan");
-                    $("small:last").text("Password tidak sama");
-                    $("small:eq(2)").addClass("pSuccess");
-                    return false;
-                } else {
-                    $("#password , #password2").addClass("success");
-                    $("small:eq(2)").addClass("pSuccess");
-                }
-            });
-
-        });
-    </script>
+    <script src="js/signup.js"></script>
 </body>
 
 </html>
